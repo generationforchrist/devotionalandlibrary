042c08514ddd7f5d1c24c75834a1e856ac56566b
generationforchrist <148167091+generationforchrist@users.noreply.github.com>
2023-10-17 01:00:21 +0100

    Add files via upload

---
+++ welcomevideo.js
@@ -0,0 +1,16 @@
+let slideIndex = 0;
+
+function showSlides() {
+    const slides = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 7 seconds (adjust as needed)
+}
+
+showSlides();
